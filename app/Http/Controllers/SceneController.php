94d57e59077d49f8e7316ff16c213681ca7658b7
Seffffff523 <yousefabayan123@gmail.com>
2026-07-07 16:03:32 +0800

    update

--- app/Http/Controllers/SceneController.php
+++ app/Http/Controllers/SceneController.php
@@ -583,7 +583,7 @@ class SceneController extends Controller
     bgcolor=\"0xffffff\" bgroundedge=\"35\" alpha=\"1\" bgalpha=\"1\" flowspacing=\"5\"
     keep=\"true\" scale=\".495\" isFilterbrgy=\"true\" linkedscene=\"scene_{$sceneId}\" publish=\"{$publish}\"
     barangay=\"{$barangay}\" categories=\"{$safeCategory}\"{$isCategoryAttr} enabled=\"true\" onclick=\"navigation();filter_init();\">
-    <layer name=\"text_{$safeTitle}\" type=\"text\" text=\"{$text}\" width=\"100%\" autoheight=\"true\"
+    <layer name=\"textni{$safeTitle}\" type=\"text\" text=\"{$text}\" width=\"100%\" autoheight=\"true\"
         align=\"bottom\" bgcolor=\"0x000000\" bgalpha=\"0\"
         css=\"color:#FFFFFF; font-size:300%; font-family:Chewy; padding-left:20px; text-align:bottom;\"/>
 </layer>
@@ -1537,7 +1537,7 @@ public function update(Request $request, $id, ScenePipelineService $pipeline)
             // Inject name as the first attribute. Callback form avoids
             // $-backreference / backslash interpretation in the replacement.
             $childTag = preg_replace_callback('/^<layer\b/i', function () use ($parentName) {
-                return '<layer name="text_' . $parentName . '"';
+                return '<layer name="textni' . $parentName . '"';
             }, $childTag, 1);
 
             $nameCount++;
